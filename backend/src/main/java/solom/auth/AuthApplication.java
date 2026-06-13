5e6187ce2c95cf5800658ccbb76f36ba798282d7
Omkar Istalkar <omkar.istalkar@gmail.com>
2026-06-13 16:51:36 +0000

    Added backend setup

---
+++ backend/src/main/java/solom/auth/AuthApplication.java
@@ -0,0 +1,14 @@
+package solom.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+		System.out.println("Backend setup complete");
+	}
+
+}
